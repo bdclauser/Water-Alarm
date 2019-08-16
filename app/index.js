8cd49f046f6b91eb45d6d298ffb138843b2b44f4
bdclauser <pzljug@hotmail.com>
2019-08-16 01:38:24 -0400

    update app and companion

--- app/index.js
+++ app/index.js
@@ -1,126 +1,161 @@
-var documentPath;
-var clockPath;
+import { readFileSync, writeFileSync } from "fs"
 
-// Fitbit does not support process
-try {
-    // Testing environment
-    documentPath = process.env.JEST ? "../tests/document" : "document";
-    clockPath = process.env.JEST ? "../tests/clock" : "clock";
-} catch (error) {
-    // Production environment
-    documentPath = "document";
-    clockPath = "clock";
+const debug = false
+
+const get_events = () => {
+    try {
+        return readFileSync("_schedule_events", "cbor")
+    } catch (error) {
+        debug && console.log(error)
+        return {}
+    }
 }
 
-const document = require(documentPath);
-const clock = require(clockPath);
+const set_events = events => {
+    writeFileSync("_schedule_events", events, "cbor")
+}
 
-const busTimes = [
-    [7, '00', 'W'],
-    [8, '15', 'WE'],
-    [9, '15', 'WE'],
-    [10, '15', 'WE'],
-    [11, '15', 'WE'],
-    [12, '15', 'WE'],
-    [13, '15', 'WE'],
-    [14, '15', 'WE'],
-    [15, '15', 'WX-E'],
-    [15, '45', 'W'],
-    [16, '15', 'E'],
-    [16, '15', 'WX'],
-    [16, '45', 'W'],
-    [17, '00', 'WX'],
-    [17, '15', 'E'],
-    [17, '20', 'WX'],
-    [17, '35', 'WX'],
-    [17, '50', 'WX'],
-    [18, '05', 'WX'],
-    [18, '15', 'E'],
-    [18, '20', 'WX'],
-    [18, '30', 'WX'],
-    [18, '45', 'WX'],
-    [19, '00', 'WX'],
-    [19, '15', 'WX-E'],
-    [19, '45', 'W'],
-    [20, '15', 'WX-E'],
-    [20, '45', 'W'],
-    [21, '15', 'WE'],
-    [22, '15', 'W'],
-    [23, '00', 'E'],
-    [23, '15', 'W']
-];
+//====================================================================================================
+// Functions
+//====================================================================================================
 
-const functions = {
-    isWeekend: function (forDay) {
-        const day = forDay.getDay();
-        return (day === 6) || (day === 0);
-    },
-    filterTimes: function (forDay, times) {
-        let result = [];
-        const weekend = functions.isWeekend(forDay);
+const add = (event) => {
+    debug && console.log("Add Event --> " + JSON.stringify(event))
+    // Set defaults
+    event = event || {}
+    event.due = event.due || Date.now()
+    event.tolerance = event.tolerance || 900000
+    // Set internal values
+    event._id = id = Math.floor(Math.random() * 10000000000000000) // Random 16-digit number
+    event._timeout_id = trigger(event)
+    // If the timeout is valid
+    if (event._timeout_id) {
+        const events = get_events()
+        events[event._id] = event
+        set_events(events)
+        return event._id
+    } else {
+        return false
+    }
+}
 
-        const hours = forDay.getHours();
-        const minutes = forDay.getMinutes();
+const remove = id => {
+    debug && console.log("Remove Event --> " + JSON.stringify(event))
+    const events = get_events()
+    // If an ID is specified
+    if (id && events[id]) {
+        // Clear the event timeout
+        clearTimeout(events[id]._timeout_id)
+        // Remove the event
+        delete events[id]
+        set_events(events)
+    }
+    // If no ID is specified
+    else {
+        // Clear all event timeouts
+        for (let id in events) {
+            clearTimeout(events[id]._timeout_id)
+        }
+        // Remove all events
+        set_events({})
+    }
+}
 
-        times.forEach(function (element) {
-            const elementMinutes = parseInt(element[1]);
-            const elementHours = element[0];
-
-            if ((elementHours > hours) || ((elementHours === hours) && (elementMinutes > minutes))) {
-                var attr = element[2];
-
-                let weekendSchedule = /E/.test(attr);
-                let weekdaySchedule = /W/.test(attr);
-
-                if (weekend) {
-                    if (weekendSchedule) {
-                        result.push(element);
-                    }
-                } else {
-                    if (weekdaySchedule) {
-                        result.push(element);
+const trigger = event => {
+    debug && console.log("Trigger --> " + JSON.stringify(event))
+    let due = event.due
+    let timeout = 9999999999999
+    // If this is a non-recurring event
+    if (typeof event.due == "number") {
+        timeout = event.due - Date.now()
+    }
+    // If this is a recurring event that was previously triggered
+    else if (event._due) {
+        timeout = event._due - Date.now()
+    }
+    // If this is a recurring event that was not previously triggered
+    else {
+        const now = new Date()
+        const times = event.due.times || ["12:00:"]
+        const days = event.due.days || [0, 1, 2, 3, 4, 5, 6]
+        let min_difference = 9999999999999
+        // Iterate over the next seven days
+        for (let i = 0; i <= 7; i++) {
+            // Create a test date
+            const test_date = new Date()
+            test_date.setDate(test_date.getDate() + i)
+            // If the test date lands on one of the scheduled days
+            if (days.indexOf(test_date.getDay()) > -1) {
+                // Iterate over the scheduled times
+                for (let time of times) {
+                    const [hours, minutes] = time.split(":")
+                    // Set the time of the test date
+                    test_date.setHours(hours)
+                    test_date.setMinutes(minutes)
+                    test_date.setSeconds(0)
+                    test_date.setMilliseconds(0)
+                    // Calculate the difference between now and the test date
+                    const difference = test_date - now
+                    // If the test date is the nearest future date encountered so far
+                    if (difference > 0 && difference < min_difference) {
+                        due = test_date.getTime()
+                        min_difference = difference
                     }
                 }
             }
-        });
-        return (result);
-    },
-    format: function (slot, forDate) {
-        const weekend = functions.isWeekend(forDate);
-        var express = weekend ? /EX/.test(slot[2]) : /WX/.test(slot[2]);
-        var attr = express ? 'X' : '';
-        return (`${slot[0]}:${slot[1]}${attr}`);
-    },
-    display: function (times, forDate) {
-        let lines = [];
-        let index = 1;
-        times.forEach(function (element) {
-            let line = document.getElementById(`line${index}`);
-            line.text = functions.format(element, forDate);
-            lines.push(line);
-            index++;
-        });
-        return (lines);
-    },
-    clearScreen: function () {
-        let index = 1;
-        for (index = 1; index < 8; index++) {
-            let line = document.getElementById(`line${index}`);
-            line.text = "";
         }
+        timeout = min_difference
     }
-};
+    // Set the internal due date (always a UNIX timestamp in milliseconds)
+    event._due = due
+    // Set the timer
+    const timeout_id = setTimeout(() => {
+        const difference = Date.now() - event._due
+        if (difference > 0 || Math.abs(difference) < event.tolerance) {
+            schedule.ondue(event)
+        } else {
+            schedule.onmissed(event)
+        }
+        // If this is a recurring event
+        if (typeof event.due == "object") {
+            // Reset the internal due date
+            delete event._due
+            // Retrigger the event
+            trigger(event)
+        }
+        // If this is a non-recurring event
+        else {
+            // Remove the event
+            remove(event._id)
+        }
+    }, timeout)
+    return timeout_id
+}
 
-functions.clearScreen();
+//====================================================================================================
+// Startup
+//====================================================================================================
 
-clock.granularity = "minutes"; // seconds, minutes, hours
-clock.ontick = () => {
-    const forDate = new Date();
-    var times = functions.filterTimes(forDate, busTimes);
-    return (functions.display(times, forDate));
-};
+const events = get_events()
+debug && console.log("Cached Events --> " + JSON.stringify(events))
 
-// Fitbit does not support module
-try {
-    module.exports = functions;
-} catch (error) { }
+// Retrigger all cached events
+for (let id in events) {
+    debug && console.log("Retrigger Event --> " + JSON.stringify(events[id]))
+    events[id]._timeout_id = trigger(events[id])
+}
+
+set_events(events)
+
+//====================================================================================================
+// Export
+//====================================================================================================
+
+const schedule = {
+    add: add,
+    remove: remove,
+    ondue: () => { },
+    onmissed: () => { }
+}
+
+export default schedule
